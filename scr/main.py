6cb8ba48f1510cd40fd209f700635bacb2757ed0
Thiago Finelon <thiago.finelon@outlook.com>
2019-10-26 22:09:16 -0400

    Adding menu to select a table to export

--- scr/main.py
+++ scr/main.py
@@ -2,9 +2,8 @@ import csv
 import boto3
 import json as json
 import decimal
-import pprint
+import pprint as pp
 import ast
-import pandas as pd
 from boto3.dynamodb.conditions import Key, Attr
 
 class DecimalEncoder(json.JSONEncoder):
@@ -13,42 +12,70 @@ class DecimalEncoder(json.JSONEncoder):
             return float(o)
         return super(DecimalEncoder, self).default(o)
 
+def main():
+    print('1. Single table')
+    print('2. Multiple tables')
+    input_select = eval(input('Select the type of export: '))
+    if int(input_select) == 1:
+        single_table()
+    elif int(input_select) == 2:
+        mult_tables()
+    else:
+        print('Invalid option!')
 
-day_mon_year = eval(input('Set a timestamp (yyyymmdd): '))
+
+def single_table():
+    client = boto3.client('dynamodb')
+    response_from_list = client.list_tables()
+    
+    for ele in enumerate(response_from_list['TableNames']): 
+        print (ele) 
+
+    table_select = eval(input('Wich table: '))
+    table_name = str(response_from_list['TableNames'][int(table_select)])
+
+    dynamodb = boto3.resource('dynamodb')
+    table    = dynamodb.Table(table_name)
+    
+    describe = client.describe_table(
+        TableName = table_name
+    )
+
+    primary_key = eval(input('Set the primary key for search(' + describe['Table']['AttributeDefinitions'][0]['AttributeName'] + '): '))
+    
+    print('Querying from table "' + str(response_from_list['TableNames'][int(table_select)]) + '" ...')
+    response = table.query(
+        KeyConditionExpression=Key(str(describe['Table']['AttributeDefinitions'][0]['AttributeName'])).eq(int(primary_key))
+    )
+    
+    
+    query_len = len(response['Items'])
+    print('Query has fineshed with ' + str(query_len) + ' items!')
+
+    json_load = query_len * [0]
+
+    for i in range(0, query_len):   
+        json_load[i] = ast.literal_eval((json.dumps(response['Items'][i], cls=DecimalEncoder)))
 
 
-dynamodb = boto3.resource('dynamodb')
-table    = dynamodb.Table('ambientais_ufms')
+ 
+#    csv_table = csv.writer(open(str(response_from_list['TableNames'][int(table_select)]) + '-' + str(primary_key) + '.csv', '+w', newline=''),  delimiter=';', quotechar='"', quoting=csv.QUOTE_NONE, escapechar='\\')
+    csv_table = csv.writer(open(str(response_from_list['TableNames'][int(table_select)]) + '-' + str(primary_key) + '.csv', '+w', newline=''))
+
+    csv_table.writerow(response['Items'][0].keys())
+
+    for row in json_load:
+        csv_table.writerow(list(row.values()))
+    
+    print('CSV created with success!')
+   
+
+def mult_tables():
+    client = boto3.client('dynamodb')
+    response_from_list = client.list_tables()
+    
+    for ele in enumerate(response_from_list['TableNames']): 
+        print (ele) 
 
 
-print('Querying from table "ambientais_ufms"...')
-response = table.query(
-    KeyConditionExpression=Key('dia_mes_ano').eq(day_mon_year)
-)
-query_len = len(response['Items'])
-print('Query has fineshed with ' + str(query_len) + ' items!')
-
-json_load = query_len * [0]
-for i in range(0, query_len):
-    json_load[i] = ast.literal_eval((json.dumps(response['Items'][i], cls=DecimalEncoder)))
-
-csv_table = csv.writer(open('station_' + str(day_mon_year) + '.csv', '+w'))
-csv_table.writerow(["dia_mes_ano","hora_minuto","rainfall","irr","temp","vento_dir","vento_vel","massaPM1","massaPM2","massaPM4","massaPM10","numPM1","numPM2","numPM4","numPM10","tamanho_medio"])
-for json_load in json_load:
-    if((json_load.hora_minuto / 10)  % 15 == 0)
-        csv_table.writerow( [json_load["dia_mes_ano"], 
-                        json_load["hora_minuto"], 
-                        json_load["rainfall"], 
-                        json_load["irr"], 
-                        json_load["temp"], 
-                        json_load["vento_dir"], 
-                        json_load["vento_vel"], 
-                        json_load["massaPM1"], 
-                        json_load["massaPM2"], 
-                        json_load["massaPM4"], 
-                        json_load["massaPM10"], 
-                        json_load["numPM1"], 
-                        json_load["numPM2"], 
-                        json_load["numPM4"], 
-                        json_load["numPM10"], 
-                        json_load["tamanho_medio"]])
+main()
